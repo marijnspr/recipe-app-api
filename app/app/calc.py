06991089d58ae540867aeb61b41764d95ca32c8f
Marijn Sprukkelhorst <marijnspr@gmail.com>
2025-09-30 13:00:48 +0200

    Weer opgepakt na 2 maanden niks

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+""" 
+Calculator functions
+"""
+
+
+def add(x, y):
+    """add x and y and return result"""
+    return x + y
+
+def substract(x,y):
+    """remove x from y and return result"""
+    return y - x 
+
