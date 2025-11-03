9d2663ed98d0f5d74b749269e6db0c6ed5afbee4
Marijn Sprukkelhorst <marijnspr@gmail.com>
2025-11-03 12:16:58 +0100

    fix: correct typos and formatting in settings, urls, admin, models, and test files

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
     'rest_framework',
-    'drf-spectacular',
+    'drf_spectacular',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -132,6 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
 
-REST_FRAMWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'spectacular.openapi.AutoSchema',
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
--- app/core/models.py
+++ app/core/models.py
@@ -13,9 +13,9 @@ class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
-        """create, save and return new user."""
+        """Create, save and return a new user."""
         if not email:
-            raise ValueError('User must have an emailadress.')
+            raise ValueError('User must have an email adress.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -23,7 +23,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """Create and return a new superuser"""
+        """Create and return a new superuser."""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -32,7 +32,7 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User (AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
