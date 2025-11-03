0ac0295747b3fb9b36dd64dff5e7acef1e0f599e
Marijn Sprukkelhorst <marijnspr@gmail.com>
2025-11-03 12:57:18 +0100

    code checker user api

---
+++ app/user/serializers.py
@@ -0,0 +1,20 @@
+"""
+Serializers for the user API view.
+"""
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user object"""
+
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'password', ' name']
+        extra_kwars = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        """Create and return a user with encypted password"""
+        return get_user_model().objects.create_user(**validated_data)
+
---
+++ app/user/views.py
@@ -0,0 +1,10 @@
+"""
+Views for the user API
+"""
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+class createUserView(generics.CREATEAPIView):
+    "Create a new user in the system."
+    serializer_class = UserSerializer
